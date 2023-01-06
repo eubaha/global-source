d4c93097eec5f5291ffe5964e3d19af11b68df3e
EC2 Default User <ec2-user@ip-172-31-34-216.us-east-2.compute.internal>
2023-01-06 23:25:44 +0000

    modified

--- app.java
+++ app.java
@@ -2,3 +2,6 @@
 # Please do not hard code
 # Make use of variables and dynamic variables
 # Author: Eno
+Welcome to Millenial technologies where we take your fintech projects to the next level of automation.
+We are a group of highly skilled and experienced DeVops engineers.
+Contact us and watch your business grow at eponential rate
